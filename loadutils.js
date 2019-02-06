dac9e47dc5f1905582cd66ece450cfc0ae7667bb
hamarb123 <hamarb123@gmail.com>
2019-02-06 19:24:55 +1100

    hamarb123 Utils update

--- loadutils.js
+++ loadutils.js
@@ -1,5 +1,7 @@
 var urlprefix = "utils/changelog/"
 
+loadversion(urlprefix, "26_");
+loadversion(urlprefix, "26");
 loadversion(urlprefix, "18");
 loadversion(urlprefix, "17");
 loadversion(urlprefix, "10");
